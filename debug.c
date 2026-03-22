6a54de4053205786b43206c476912b772ee11669
VedantParte23 <hugstonalbert@gmail.com>
2026-03-22 16:38:11 +0530

    added comments for more understanding also remove double new lines cause it was looking odd

--- debug.c
+++ debug.c
@@ -9,51 +9,51 @@ static unsigned int main_debug = 0;
 void give_info(unsigned int off)
 {
 
-    main_debug = off;
+        main_debug = off;
 
-    if (main_debug)
-    {
+        if (main_debug)
+        {
 #ifdef __clang__
-        printf("You are using a clang compilter");
+                printf("You are using a clang compilter");
 
 #elif defined __GNUC__
-        printf("You are using GNU c compilter");
+                printf("You are using GNU c compilter");
 
 #elif defined _MSC_VER
-        printf("You are using MS visual cpp compiler");
+                printf("You are using MS visual cpp compiler");
 
 #endif // DEBUG
 
 #ifdef __linux__
-        printf("\n\nYour on a linux machine");
+                printf("\nYour on a linux machine");
 
 #elif defined _WIN32
-        printf("\n\nYour on a windows machine");
+                printf("\nYour on a windows machine");
 
 #elif defined __APPLE__
-        printf("\n\nYour on a MAC (Rich guy 😭)");
+                printf("\nYour on a MAC (Rich guy 😭)");
 
 #endif
 
 #ifdef __STDC_VERSION__
 
 #if __STDC_VERSION__ == 199901L
-        printf("\n\nThis is C99 version of c");
+                printf("\nThis is C99 version of c");
 #elif __STDC_VERSION__ == 201112L
-        printf("\n\nThis is C11 version of c");
+                printf("\nThis is C11 version of c");
 #elif __STDC_VERSION__ == 201710L
-        printf("\n\nThis is C17/18 version of c");
+                printf("\nThis is C17/18 version of c");
 #elif __STDC_VERSION__ == 202311L
-        printf("\n\nThis is C23 version of c");
+                printf("\nThis is C23 version of c");
 #else
-        printf("\n\nYou are using a ancient compilter please upgrade it");
+                printf("\nYou are using a ancient compilter please upgrade it");
 #endif
 
 #endif // DEBUG
 #if (__STDC_IEC_559__ == 1)
-        printf("\n\ncompiler per-forms floating-point arithmetic according to the IEC 60559 standard");
+                printf("\ncompiler per-forms floating-point arithmetic according to the IEC 60559 standard");
 #endif
 
-        printf("\n\nCompilted on %s at %s", __DATE__, __TIME__);
-    }
+                printf("\nCompilted on %s at %s", __DATE__, __TIME__);
+        }
 }
--- main.c
+++ main.c
@@ -11,21 +11,25 @@ int main(int argc, char *argv[]){
     if(argc> 1){
         debug = atoi(argv[1]);
     }
+    //gives info about envirenenet variables
     give_info(debug);
     //You can make 
     //unsigned int debug = 1; to automaticly use it without argument
 
 
-    //your code goes here:
+    //your code goes here,this is sample code:
     int *ptr = malloc(3*sizeof(int));
     int *ptr_second = malloc(100);
 
+    //just freed ptr in print repost ptr_second should be seen are leaked
+    
     free(ptr);
     // to here
 
 
 
-    
+
+    //prints report on memory leaks.
     print_report();
 
     return 0;
