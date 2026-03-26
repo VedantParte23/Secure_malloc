136d9876d7bd9bc3f051da092062662a0898e19a
VedantParte23 <hugstonalbert@gmail.com>
2026-03-26 18:19:11 +0530

    Final cleanup

--- main.c
+++ main.c
@@ -5,34 +5,37 @@
 
 // pass argument 1 for debug
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     unsigned int debug = 0;
-    if(argc> 1){
+    if (argc > 1)
+    {
         debug = atoi(argv[1]);
     }
-    //gives info about envirenenet variables
+    // gives info about envirenenet variables
     give_info(debug);
-    //You can make 
-    //unsigned int debug = 1; to automaticly use it without argument
+    // You can make
+    // unsigned int debug = 1; to automaticly use it without argument
 
 
-    //your code goes here,this is sample code:
-    int *ptr = malloc(3*sizeof(int));
+
+
+    // your code goes here,this vv
+    // is sample code:
+    int *ptr = malloc(3 * sizeof(int));
     int *ptr_second = malloc(100);
 
-    //just freed ptr in print repost ptr_second should be seen are leaked
-    
+    // just freed ptr in print repost ptr_second should be seen are leaked
+
     free(ptr);
-    // to here
+    // to here ^^
 
 
 
 
-    //prints report on memory leaks.
+
+    // prints report on memory leaks.
     print_report();
 
     return 0;
 }
-
-
